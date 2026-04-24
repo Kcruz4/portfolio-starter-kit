a7a6b4e71f070b4db66f31269ae5e7321b3a0660
Karen Cruz <karencruz200104@gmail.com>
2026-04-24 23:23:00 +1000

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -52,8 +52,8 @@ export default function RootLayout({
         GeistMono.variable
       )}
     >
-      <body className="antialiased max-w-xl mx-4 mt-8 lg:mx-auto">
-        <main className="flex-auto min-w-0 mt-6 flex flex-col px-2 md:px-0">
+      <body className="antialiased m-0">
+        <main className="w-full min-w-0">
           <Navbar />
           {children}
           <Footer />
